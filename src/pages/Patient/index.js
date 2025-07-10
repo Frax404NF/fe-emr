ccbe684047c175b04b6ca1004e6f1f31e85b952d
Frandi <frandicollage21@gmail.com>
2025-07-10 15:33:25 +0800

    refactor: remove unused Patient page components
    Menghapus file-file yang tidak digunakan lagi di direktori pages/Patient karena fitur terkait telah diimplementasikan ulang atau digabungkan ke dalam komponen lain.

--- src/pages/Patient/index.js
+++
@@ -1,3 +0,0 @@
-// Patient Management Components Export
-export { default as InpatientDetailPatient } from './InpatientDetailPatient';
-export { default as PatientVisitDetail } from './PatientVisitDetail';
